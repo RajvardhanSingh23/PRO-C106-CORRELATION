c506fe56c8af5619587bfbdee14cf7872d3d0b07
RajvardhanSingh23 <90671342+RajvardhanSingh23@users.noreply.github.com>
2021-10-23 16:58:20 +0530

    Update coffee.py

--- coffee.py
+++ coffee.py
@@ -2,7 +2,7 @@ import numpy as np
 import csv
 import pandas as pd 
 import plotly.express as px
-with open("coffee.csv") as f:
+with open("cups_of_coffee_vs_hours_of_sleep.csv") as f:
     df = csv.DictReader(f)
     fig = px.scatter(df, x="Coffee in ml", y="sleep in hours")
     fig.show()
@@ -19,7 +19,7 @@ def findCorrelation(dataSource):
     correlation = np.corrcoef(dataSource["x"], dataSource["y"])
     print("Correlation Between Coffee in ml vs sleep in hours:- ", correlation[0,1])
 def setUp():
-    data_path = "coffee.csv"
+    data_path = "cups_of_coffee_vs_hours_of_sleep.csv"
     dataSource = getDataSource(data_path)
     findCorrelation(dataSource)
 setUp()
